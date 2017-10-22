396fc1af92ab5d585a55c44d66e95c264517531e
Jon Loeffelholz <Jon.Loeffelholz@agropur.com>
2017-10-22 17:07:47 -0500

    Created models.

---
+++ MealPlanner.Data/Models/Ingredient.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MealPlanner.Data.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int IngredientCategoryId { get; set; }
+
+        public int StoreId { get; set; }
+
+        public virtual IngredientCategory IngredientCategory { get; set; }
+
+        public virtual Store Store { get; set; }
+    }
+}
---
+++ MealPlanner.Data/Models/RecipeDetail.cs
@@ -0,0 +1,21 @@
+﻿namespace MealPlanner.Data.Models
+{
+    public class RecipeDetail
+    {
+        public int Id { get; set; }
+
+        public int RecipeId { get; set; }
+
+        public int IngredientId { get; set; }
+
+        public decimal Quantity { get; set; }
+
+        public int UnitId { get; set; }
+
+        public virtual Recipe Recipe { get; set; }
+
+        public virtual Ingredient Ingredient { get; set; }
+
+        public virtual Unit Unit { get; set; }
+    }
+}
