a35d2197cbf8b7289a4fa5665f08bb712eb8f982
Jon Loeffelholz <loeffelh.jona@uwlax.edu>
2017-10-22 16:39:53 -0500

    initial commit

---
+++ MealPlanner.Data/Models/Ingredients.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MealPlanner.Data.Models
+{
+    public class Ingredients
+    {
+        public int Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public int IngredientCategoryId { get; set; }
+    }
+}
---
+++ MealPlanner.Data/Models/MealPlannerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MealPlanner.Data.Models
+{
+    public class MealPlannerContext : DbContext
+    {
+        public MealPlannerContext(DbContextOptions<MealPlannerContext> options) : base(options)
+        {
+        }
+    }
+}
