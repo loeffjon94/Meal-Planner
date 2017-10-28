e2f10ad1fe5fcb3d85b5729a466727f8ae4edd34
Jon Loeffelholz <loeffelh.jona@uwlax.edu>
2017-10-28 09:49:05 -0500

    Wired up backend.

--- MealPlanner.Data/Models/MealPlannerContext.cs
+++ MealPlanner.Data/Models/MealPlannerContext.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MealPlanner.Data.Models
 {
@@ -9,6 +6,53 @@ namespace MealPlanner.Data.Models
     {
         public MealPlannerContext(DbContextOptions<MealPlannerContext> options) : base(options)
         {
+        }
+
+        public virtual DbSet<MealPlanner.Data.Models.Ingredient> Ingredients { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.IngredientCategory> IngredientCategories { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.Recipe> Recipes { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.RecipeCategory> RecipeCategories { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.RecipeDetail> RecipeDetails { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.Store> Stores { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.Unit> Units { get; set; }
+        public virtual DbSet<MealPlanner.Data.Models.Image> Images { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MealPlanner.Data.Models.Image>()
+                .HasMany(p => p.Recipes)
+                .WithOne(p => p.Image)
+                .HasForeignKey(p => p.ImageId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.IngredientCategory>()
+                .HasMany(p => p.Ingredients)
+                .WithOne(p => p.IngredientCategory)
+                .HasForeignKey(p => p.IngredientCategoryId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.Store>()
+                .HasMany(p => p.Ingredients)
+                .WithOne(p => p.Store)
+                .HasForeignKey(p => p.StoreId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.RecipeCategory>()
+                .HasMany(p => p.Recipes)
+                .WithOne(p => p.RecipeCategory)
+                .HasForeignKey(p => p.RecipeCategoryId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.Recipe>()
+                .HasMany(p => p.RecipeDetails)
+                .WithOne(p => p.Recipe)
+                .HasForeignKey(p => p.RecipeId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.Ingredient>()
+                .HasMany(p => p.RecipeDetails)
+                .WithOne(p => p.Ingredient)
+                .HasForeignKey(p => p.IngredientId);
+
+            modelBuilder.Entity<MealPlanner.Data.Models.Unit>()
+                .HasMany(p => p.RecipeDetails)
+                .WithOne(p => p.Unit)
+                .HasForeignKey(p => p.UnitId);
         }
     }
 }
---
+++ MealPlanner/Controllers/BaseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MealPlanner.Data.Models;
+using MealPlanner.Data.Repos;
+
+namespace MealPlanner.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly MealPlannerContext _context;
+        protected readonly MealsRepo _mealsRepo;
+
+        public BaseController(MealPlannerContext context)
+        {
+            _context = context;
+            _mealsRepo = new MealsRepo(context);
+        }
+    }
+}
