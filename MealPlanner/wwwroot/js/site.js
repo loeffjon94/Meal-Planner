db06a64fbe149e957e2e0cbf58384acce4c8f80a
Jon Loeffelholz <Jon.Loeffelholz@agropur.com>
2017-10-28 19:21:37 -0500

    Added swipe to carousel.

--- MealPlanner/wwwroot/js/site.js
+++ MealPlanner/wwwroot/js/site.js
@@ -1,1 +1,7 @@
-﻿// Write your JavaScript code.
+﻿$('.carousel').swipe({
+    swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+        if (direction == 'left') $(this).carousel('next');
+        if (direction == 'right') $(this).carousel('prev');
+    },
+    allowPageScroll: "vertical"
+})
