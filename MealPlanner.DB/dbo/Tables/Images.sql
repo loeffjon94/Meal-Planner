d5cda7420cb24ecd29a5376120644eb0aa7b6151
Jon Loeffelholz <loeffelholz.jon@gmail.com>
2019-01-12 15:44:49 -0600

    Fixed database schema and removed prod error handling.

--- MealPlanner.DB/dbo/Tables/Images.sql
+++ MealPlanner.DB/dbo/Tables/Images.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Images] (
     [Id]      INT             IDENTITY (1, 1) NOT NULL,
-    [Data]    VARBINARY (MAX) NOT NULL,
+    [Data]    VARBINARY (MAX) NULL,
     [DataUrl] NVARCHAR (4000) NULL,
     CONSTRAINT [PK_Images] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
--- MealPlanner/Startup.cs
+++ MealPlanner/Startup.cs
@@ -28,15 +28,8 @@ namespace MealPlanner
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
 
             //app.UseDeveloperExceptionPage();
             //app.UseBrowserLink();
